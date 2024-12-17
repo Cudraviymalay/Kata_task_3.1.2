e474d1d720d08c831169c6293ef449fe733fc421
Mahmutov Azat <cudraviymalay@gmail.com>
2024-12-17 18:02:47 +0300

    рабочая версия из склонированного проекта

---
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -0,0 +1,74 @@
+package ru.kata.spring.boot_security.demo.models;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @javax.persistence.Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    private String name;
+
+    private String surname;
+
+    private int age;
+
+    public User() {
+    }
+
+    public User(int id, String name, String surname, int age) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
